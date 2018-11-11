c3665abbe6396e6437b779b1e954e82cdc86669e
Chung Wei Leong <leongchungwei@hotmail.com>
2018-11-12 00:01:11 +0800

    Added UWP app project

---
+++ MixBuild.Uwp/Views/ImportImagePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MixBuild.Uwp.Views
+{
+    public sealed partial class ImportImagePage : Page
+    {
+        public ImportImagePage()
+        {
+            this.InitializeComponent();
+            BackButton.Click += (s, e) =>
+            {
+                Frame rootFrame = Window.Current.Content as Frame;
+                if (rootFrame.CanGoBack) rootFrame.GoBack();
+            };
+
+            //setup title bar draggable region
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            AppTitleBar.Height = coreTitleBar.Height;
+            coreTitleBar.LayoutMetricsChanged += (s, args) => AppTitleBar.Height = coreTitleBar.Height;
+
+            Window.Current.SetTitleBar(AppTitleBar);
+        }
+    }
+}
---
+++ MixBuild.Uwp/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MixBuild.Uwp.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            GetStartedButton.Click += (s, e) => this.Frame.Navigate(typeof(ImportImagePage));
+
+            //remove titlebar
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            //setup title bar draggable region
+            AppTitleBar.Height = coreTitleBar.Height;
+            coreTitleBar.LayoutMetricsChanged += (s, args) => AppTitleBar.Height = coreTitleBar.Height;
+
+            Window.Current.SetTitleBar(AppTitleBar);
+        }
+    }
+}
